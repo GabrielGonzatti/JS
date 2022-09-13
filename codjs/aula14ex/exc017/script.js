a07e76e15eddd7194cded70910839dbe8e11c800
Gabriel Gonzatti <109048186+GabrielGonzatti@users.noreply.github.com>
2022-09-13 11:58:49 -0300

    update exc17

---
+++ codjs/aula14ex/exc017/script.js
@@ -0,0 +1,11 @@
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let selet = document.getElementById('seltab')
+    if (num.value.length == 0){
+        window.alert('ERRO')
+    }else{
+        let n = Number(num.value)
+        
+    }
+    
+}
