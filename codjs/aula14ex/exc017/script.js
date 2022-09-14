d88e6261f44d5bfa2657e19e68cef2ba333de77c
Gabriel Gonzatti <109048186+GabrielGonzatti@users.noreply.github.com>
2022-09-14 00:14:02 -0300

    update

--- codjs/aula14ex/exc017/script.js
+++ codjs/aula14ex/exc017/script.js
@@ -5,7 +5,13 @@ function tabuada(){
         window.alert('ERRO')
     }else{
         let n = Number(num.value)
-        
+        let c = 1
+        while (c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }
     }
     
 }
