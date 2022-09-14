64e5b904f9073e3e1d0d35d678bc1d609b350b8f
Gabriel Gonzatti <109048186+GabrielGonzatti@users.noreply.github.com>
2022-09-14 10:56:40 -0300

    finally exc017

--- codjs/aula14ex/exc017/script.js
+++ codjs/aula14ex/exc017/script.js
@@ -1,12 +1,13 @@
 function tabuada(){
     let num = document.getElementById('txtn')
-    let selet = document.getElementById('seltab')
-    if (num.value.length == 0){
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
         window.alert('ERRO')
-    }else{
+    } else {
         let n = Number(num.value)
         let c = 1
-        while (c <= 10){
+        tab.innerHTML = ('Olá')
+        while (c <= 10) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             tab.appendChild(item)
