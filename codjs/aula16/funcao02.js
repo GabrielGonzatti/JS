362f792ce8c8acf0c96d421a0a6cce643bbab3d1
Gabriel Gonzatti <109048186+GabrielGonzatti@users.noreply.github.com>
2022-09-23 11:59:02 -0300

    funcao 02,03,04 JS

---
+++ codjs/aula16/funcao02.js
@@ -0,0 +1,6 @@
+function soma(n1=0,n2=0){
+    return n1 + n2
+}
+console.log(soma(2,8))
+
+//ao declarar os parametros, pode colocar =0 para caso haja um campo vazio, o js irá declarar o parametro como zero.
