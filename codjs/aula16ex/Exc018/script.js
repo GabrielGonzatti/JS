75f2164e0a8b5bd8a33bda87d0e0c5667d8df05f
Gabriel Gonzatti <109048186+GabrielGonzatti@users.noreply.github.com>
2022-09-25 19:32:04 -0300

    update final

--- codjs/aula16ex/Exc018/script.js
+++ codjs/aula16ex/Exc018/script.js
@@ -7,10 +7,17 @@ let valores = []
 //Função principal abaixo
 function adicionar() {
     if(isNumero(num.value) && !inList(num.value, valores)){
-        window.alert('Tudo ok!')
+        valores.push(Number(num.value))
+        let item = document.createElement('Option')
+        item.text = `Valor ${num.value} adicionado!`
+        list.appendChild(item)
+        res.innerHTML = ''
     }else{
         window.alert('[ERRO] valor inválido ou já adicionado!')
     }
+    num.value = ''
+    num.focus()
+    //focus faz o cursor voltar para ele sempre que uma funcionalidade atualizar!
 }
 
 
@@ -33,3 +40,34 @@ function inList(n, l){
         return false
     }
 }
+
+//função para finalizar!
+function Finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            media = soma/tot
+            if(valores[pos] > maior){
+                maior = valores[pos]
+            }
+            if(valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor é: ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor é: ${menor}.</p>`
+        res.innerHTML += `<p>A soma dos valores é: ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores é: ${media}.</p>`
+
+    }
+}
+
