5ff9d3fc486bf6fe7a3ea3444f256f7b63a9078f
Gabriel Gonzatti <109048186+GabrielGonzatti@users.noreply.github.com>
2022-09-27 10:10:15 -0300

    create js_Hora-de-codar

---
+++ js_Hora-de-codar/3 - particularidades/script.js
@@ -0,0 +1,17 @@
+console.log("Primeiro");
+
+console.log("Último");
+
+console.log("Mais um console.log");
+//declaração variáveis
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
+
+//comentário
+
+/*
+múltiplos comentários
+*/
---
+++ js_Hora-de-codar/4 - Variaveis/script.js
@@ -0,0 +1,9 @@
+var teste = 1;
+var nome = "jose";
+
+console.log(teste);
+
+teste = ('Matheus');
+
+console.log(teste);
+console.log(nome);
