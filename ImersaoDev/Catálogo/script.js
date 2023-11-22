7d7cd5f8a5941fdb05a4008a0dfb52c08b1a37a5
Gabriel Gonzatti <109048186+GabrielGonzatti@users.noreply.github.com>
2023-11-22 10:01:14 -0300

    Update JS jogos

---
+++ ImersaoDev/Catálogo/script.js
@@ -0,0 +1,9 @@
+var Filme01 = "https://upload.wikimedia.org/wikipedia/pt/a/a1/P%C3%B4ster-Rebelde-Mexico.jpg"
+var Filme02 = "https://img.assinaja.com/assets/tZ/041/img/427092_520x520.png"
+var Filme03 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiGkputNCgsjME_OigtmqGL5FLmFI7alHzZg&usqp=CAU"
+var Filme04 = "https://imagem.natelinha.uol.com.br/original/star-wars-ganhara-novo-filme_5175.jpeg"
+
+document.write("<img src=" + Filme01 + ">");
+document.write("<img src=" + Filme02 + ">");
+document.write("<img src=" + Filme03 + ">");
+document.write("<img src=" + Filme04 + ">");
